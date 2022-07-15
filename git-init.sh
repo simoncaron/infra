ce199ab86490c7ecc2ab2f3464767d4d9466fc74
Simon Caron <simon.caron.8@gmail.com>
2022-07-14 23:41:51 -0400

    VPN Relay: Added Tailscale

--- git-init.sh
+++ git-init.sh
@@ -7,7 +7,13 @@
 if [ -d .git/ ]; then
 rm .git/hooks/pre-commit
 cat <<EOT >> .git/hooks/pre-commit
-if ( git show :vars/vault.yml | grep -q "\$ANSIBLE_VAULT;" ); then
+if ( git show :vars/vault-k3s.yml | grep -q "\$ANSIBLE_VAULT;" ); then
+echo "[38;5;108mVault Encrypted. Safe to commit.[0m"
+else
+echo "[38;5;208mVault not encrypted! Run 'make encrypt' and try again.[0m"
+exit 1
+fi
+if ( git show :vars/vault-vpn.yml | grep -q "\$ANSIBLE_VAULT;" ); then
 echo "[38;5;108mVault Encrypted. Safe to commit.[0m"
 else
 echo "[38;5;208mVault not encrypted! Run 'make encrypt' and try again.[0m"
